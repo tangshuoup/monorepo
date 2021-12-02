d64daee10607922053a852fe31a45679081136c4
tanghsuo <shuo.tang@firesoon.com>
2021-12-02 18:21:31 +0800

    feat:init

--- packages/components/.fatherrc.ts
+++ packages/components/.fatherrc.ts
@@ -4,18 +4,13 @@
  * @Author: tangshuo
  * @Date: 2021-12-02 17:59:31
  * @LastEditors: tangshuo
- * @LastEditTime: 2021-12-02 18:09:01
+ * @LastEditTime: 2021-12-02 18:19:17
  */
 export default {
-  target: 'browser',
-  esm: 'babel',
-  cjs: 'babel',
-  umd: {
-    name: 'ant-ui',
-    sourcemap: true,
-  },
-  lessInBabelMode: true, // babel 模式下做 less 编译
+  esm: 'rollup',
+  cjs: 'rollup',
   runtimeHelpers: true,
+  cssModules: { generateScopedName: '[local]___[hash:base64:5]' },
   extraBabelPlugins: [
     [
       'babel-plugin-import',
